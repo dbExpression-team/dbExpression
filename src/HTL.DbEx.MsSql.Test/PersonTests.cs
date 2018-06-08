dba553ac53de83323207385f307af0aaa74ffc48
Gary Grubbs <gary@cquentia.com>
2018-06-08 15:27:32 -0500

    started testing project

---
+++ src/HTL.DbEx.MsSql.Test/PersonTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace HTL.DbEx.MsSql.Test
+{
+    public class PersonTests : UnitTestBase
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+
+        }
+
+        [Fact]
+        public void TestMethod2()
+        {
+
+        }
+    }
+}
---
+++ src/HTL.DbEx.MsSql.Test/UnitTestBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+namespace HTL.DbEx.MsSql.Test
+{
+    public abstract class UnitTestBase
+    {
+        protected UnitTestBase()
+        {
+            var databaseName = ConfigurationManager.AppSettings["DefaultDatabase"];
+            new Seeder(databaseName, ConfigurationManager.ConnectionStrings[databaseName].ConnectionString).RunScript("Reset.sql");
+        }
+
+        protected UnitTestBase(string databaseName)
+        {
+            new Seeder(databaseName, ConfigurationManager.ConnectionStrings[databaseName].ConnectionString).RunScript("Reset.sql");
+        }
+    }
+}
