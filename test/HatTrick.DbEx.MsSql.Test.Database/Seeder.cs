10b2b0ccf5575b2329e830eb3a67a74a1ba11017
Gary Grubbs <gary@cquentia.com>
2018-11-05 18:35:36 -0600

    Testing data (database): scripts, readme, seeder.

--- test/HatTrick.DbEx.MsSql.Test.Database/Seeder.cs
+++ test/HatTrick.DbEx.MsSql.Test.Database/Seeder.cs
@@ -10,32 +10,31 @@ namespace HatTrick.DbEx.MsSql.Test.Database
     public class Seeder
     {
         private ConnectionStringSettings connectionStringSettings;
-        private string SourceDirectory => AppDomain.CurrentDomain.BaseDirectory.Replace(@"\bin\Debug", string.Empty);
 
         public Seeder(string name, string connectionString)
         {
-            connectionString = connectionString.Replace("{Path}", SourceDirectory);
             connectionStringSettings = new ConnectionStringSettings(name, connectionString);
         }
 
         public Seeder(ConnectionStringSettings connectionStringSettings)
         {
             this.connectionStringSettings = connectionStringSettings;
-            connectionStringSettings.ConnectionString = connectionStringSettings.ConnectionString.Replace("{Path}", SourceDirectory);
         }
 
         public void RunScript(string scriptName)
         {
-            RunScript(SourceDirectory, scriptName);
+            RunScript(scriptName, AppDomain.CurrentDomain.BaseDirectory.Replace(@"\bin\Debug", string.Empty));
         }
 
-        public void RunScript(string scriptPath, string scriptName)
+        public void RunScript(string scriptName, string scriptPath)
         {
-            using (var resetScriptReader = File.OpenText(Path.Combine(scriptPath, "Reset.sql")))
+            using (var resetScriptReader = File.OpenText(Path.Combine(scriptPath, scriptName)))
             using (var conn = new SqlConnection(connectionStringSettings.ConnectionString))
             {
-                var server = new Server(new ServerConnection(conn));
+                var serverConn = new ServerConnection(conn);
+                var server = new Server(serverConn);
                 server.ConnectionContext.ExecuteNonQuery(resetScriptReader.ReadToEnd());
+                serverConn.Disconnect();
             }
         }
     }
