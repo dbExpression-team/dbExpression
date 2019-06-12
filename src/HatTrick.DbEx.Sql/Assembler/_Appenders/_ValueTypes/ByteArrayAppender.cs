5d89e9c300b719a1ba0a30105087958d7236de0c
JRod-Eiman <jerrod.eiman@gmail.com>
2019-06-12 16:35:32 -0500

    byte array appender update

--- src/HatTrick.DbEx.Sql/Assembler/_Appenders/_ValueTypes/ByteArrayAppender.cs
+++ src/HatTrick.DbEx.Sql/Assembler/_Appenders/_ValueTypes/ByteArrayAppender.cs
@@ -9,13 +9,13 @@ namespace HatTrick.DbEx.Sql.Assembler
     //NOTE: JRod, not really a value type, but handled exactly the same...
     public class ByteArrayAppender : IAssemblyPartAppender<byte[]>
     {
-        public void AppendPart(object expression, ISqlStatementBuilder builder, AssemblerContext context)
+        public void AppendPart(object expression, ISqlStatementBuilder builder, AssemblyContext context)
            => AppendPart((byte[])expression, builder, context);
 
-        public void AppendPart(byte[] expression, ISqlStatementBuilder builder, AssemblerContext context)
+        public void AppendPart(byte[] expression, ISqlStatementBuilder builder, AssemblyContext context)
         {
-            if (context.CurrentField != null)
-                builder.Appender.Write(builder.Parameters.Add(expression, context.CurrentField.Field).Parameter.ParameterName);
+            if (context?.Field != null)
+                builder.Appender.Write(builder.Parameters.Add(expression, context.Field).Parameter.ParameterName);
             else
                 builder.Appender.Write(builder.Parameters.Add<byte[]>(expression).ParameterName);
         }
