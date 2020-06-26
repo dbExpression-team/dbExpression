2afd3a237517953ebd6bc9a86ec551e1a3a6902b
Gary Grubbs <gary.grubbs@gmail.com>
2020-06-26 17:12:35 -0500

    Added sample project to solution, with codegen issue in PersonService (./samples/mssql/ServerSideBlazorApp/Service/PersonService.cs)

--- samples/mssql/ServerSideBlazorApp/Service/PersonService.cs
+++ samples/mssql/ServerSideBlazorApp/Service/PersonService.cs
@@ -9,12 +9,14 @@ namespace ServerSideBlazorApp.Service
 {
     public class PersonService
     {
-        //public IEnumerable<Person> GetPeople(int offset, int length, string nameStartsWith)
-        //{
-        //    return db.SelectMany<Person>()
-        //        .From(DataService.dbo.Person)
-        //        .Where((dbo.Person.FirstName + " " + dbo.Person.LastName).Like(nameStartsWith + "%")
-        //        .Execute();
-        //}
+        public IEnumerable<Person> GetPeople(int offset, int length, string nameStartsWith)
+        {
+            return db.SelectMany<Person>()
+                .From(DataService.dbo.Person)
+                .Where((dbo.Person.FirstName + " " + dbo.Person.LastName).Like(nameStartsWith + "%")
+                .Execute();
+        }
+
+        //GWG to JROD: Here's what I put as "Application arguments" in HatTrick.DbEx.Tools to gen code in the ./Data/_Generated folder: "gen -p d:/src/db-ex/samples/mssql/ServerSideBlazorApp/DbExConfig.json -o d:/src/db-ex/samples/mssql/ServerSideBlazorApp/Data/_Generated"
     }
 }
