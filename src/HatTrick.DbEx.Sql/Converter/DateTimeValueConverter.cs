df49eb2ab5e07a5ac6fe63a9d0b1102342706469
gwgrubbs <gary.grubbs@gmail.com>
2022-11-07 12:19:34 -0600

    DateTime and DateTimeOffset conversion - Fixed issue when DateTime expressions were used with literal DateTimeOffset values and vice-versa - Added DateTime and DateTime specific value converters

---
+++ src/HatTrick.DbEx.Sql/Converter/DateTimeValueConverter.cs
@@ -0,0 +1,53 @@
+﻿#region license
+// Copyright (c) HatTrick Labs, LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// The latest version of this file can be found at https://github.com/HatTrickLabs/db-ex
+#endregion
+
+using System;
+
+namespace HatTrick.DbEx.Sql.Converter
+{
+    public class DateTimeValueConverter : ValueConverter<DateTime>, IValueConverter<DateTime>
+    {
+        public override (Type Type, object? ConvertedValue) ConvertToDatabase(object? value)
+        {
+            if (value is null)
+                throw new DbExpressionException("Expected a non-null value from the database, but received a null value.");
+
+            if (value is DateTime)
+                return (typeof(DateTime), (DateTime)value);
+
+            if (value is DateTimeOffset)
+                return (typeof(DateTime), DateTime.SpecifyKind(((DateTimeOffset)value).UtcDateTime, DateTimeKind.Utc));
+
+            return base.ConvertToDatabase(value);
+        }
+
+        public override DateTime ConvertFromDatabase(object? value)
+        {
+            if (value is null)
+                throw new DbExpressionException("Expected a non-null value from the database, but received a null value.");
+
+            if (value is DateTime)
+                return (DateTime)value;
+
+            if (value is DateTimeOffset)
+                return DateTime.SpecifyKind(((DateTimeOffset)value).UtcDateTime, DateTimeKind.Utc);
+            
+            return base.ConvertFromDatabase(value);
+        }
+    }
+}
---
+++ test/HatTrick.DbEx.MsSql.Test.Unit/Converter/DateAndTimeValueConverterTests.cs
@@ -0,0 +1,268 @@
+﻿using DbEx.Data;
+using DbEx.DataService;
+using FluentAssertions;
+using HatTrick.DbEx.MsSql.Configuration;
+using HatTrick.DbEx.Sql;
+using HatTrick.DbEx.Sql.Converter;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace HatTrick.DbEx.MsSql.Test.Unit.Converter
+{
+    public class DateAndTimeValueConverterTests : TestBase
+    {
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetime_converter_convert_to_database_a_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime>();
+            var today = DateTimeOffset.Now.Date;
+
+            //when
+            var converted = converter.ConvertToDatabase(today);
+
+            //then
+            converted.Should().Be((typeof(DateTime), today));
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetime_converter_convert_from_database_a_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime>();
+            var today = DateTimeOffset.Now.Date;
+
+            //when
+            var converted = converter.ConvertFromDatabase(today);
+
+            //then
+            converted.Should().Be(today);
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetime_converter_convert_to_database_a_null_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime?>();
+            DateTimeOffset? value = null;
+
+            //when
+            var converted = converter.ConvertToDatabase(value);
+
+            //then
+            converted.Should().Be((typeof(DateTime?), value));
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetime_converter_convert_from_database_a_null_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime?>();
+            DateTimeOffset? value = null;
+
+            //when
+            var converted = converter.ConvertFromDatabase(value);
+
+            //then
+            converted.Should().Be(null);
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetime_converter_convert_to_database_a_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime>();
+            var today = DateTime.Now.Date;
+
+            //when
+            var converted = converter.ConvertToDatabase(today);
+
+            //then
+            converted.Should().Be((typeof(DateTime), today));
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetime_converter_convert_from_database_a_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime>();
+            var today = DateTime.Now.Date;
+
+            //when
+            var converted = converter.ConvertFromDatabase(today);
+
+            //then
+            converted.Should().Be(today);
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetime_converter_convert_to_database_a_null_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime?>();
+            DateTime? value = null;
+
+            //when
+            var converted = converter.ConvertToDatabase(value);
+
+            //then
+            converted.Should().Be((typeof(DateTime?), value));
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetime_converter_convert_from_database_a_null_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTime?>();
+            DateTime? value = null;
+
+            //when
+            var converted = converter.ConvertFromDatabase(value);
+
+            //then
+            converted.Should().Be(null);
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetimeoffset_converter_convert_to_database_a_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset>();
+            var today = DateTimeOffset.Now.Date;
+
+            //when & then
+            var ex = Assert.Throws<DbExpressionException>(() => converter.ConvertFromDatabase(today));
+            ex.Message.Should().StartWith("Cannot convert a value from DateTime");
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetimeoffset_converter_convert_from_database_a_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset>();
+            var today = DateTimeOffset.Now.Date;
+
+            //when & then
+            var ex = Assert.Throws<DbExpressionException>(() => converter.ConvertFromDatabase(today));
+            ex.Message.Should().StartWith("Cannot convert a value from DateTime");
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetimeoffset_converter_convert_to_database_a_null_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset?>();
+            DateTimeOffset? value = null;
+
+            //when
+            var converted = converter.ConvertToDatabase(value);
+
+            //then
+            converted.Should().Be((typeof(DateTimeOffset?), value));
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetimeoffset_converter_convert_from_database_a_null_datetimeoffset_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset?>();
+            DateTimeOffset? value = null;
+
+            //when
+            var converted = converter.ConvertFromDatabase(value);
+
+            //then
+            converted.Should().Be(null);
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetimeoffset_converter_convert_to_database_a_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset>();
+            var today = DateTime.Now.Date;
+
+            //when
+            var converted = converter.ConvertToDatabase(today);
+
+            //then
+            converted.Should().Be((typeof(DateTimeOffset), new DateTimeOffset(today)));
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_datetimeoffset_converter_convert_from_database_a_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset>();
+            var today = DateTime.Now.Date;
+
+            //when
+            var converted = converter.ConvertFromDatabase(today);
+
+            //then
+            converted.Should().Be(today);
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetimeoffset_converter_convert_to_database_a_null_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset?>();
+            DateTime? value = null;
+
+            //when
+            var converted = converter.ConvertToDatabase(value);
+
+            //then
+            converted.Should().Be((typeof(DateTimeOffset?), value));
+        }
+
+        [Theory]
+        [MsSqlVersions.AllVersions]
+        public void Does_nullabledatetimeoffset_converter_convert_from_database_a_null_datetime_value(int version)
+        {
+            //given
+            var (db, serviceProvider) = Configure<MsSqlDb>().ForMsSqlVersion(version);
+            var converter = serviceProvider.GetServiceProviderFor<MsSqlDb>().GetRequiredService<IValueConverterFactory>().CreateConverter<DateTimeOffset?>();
+            DateTime? value = null;
+
+            //when
+            var converted = converter.ConvertFromDatabase(value);
+
+            //then
+            converted.Should().Be(null);
+        }
+    }
+}
